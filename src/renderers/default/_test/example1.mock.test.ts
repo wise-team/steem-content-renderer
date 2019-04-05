07f694fda4526907080396f02cc61be7ce89ab75
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-04-05 16:29:14 +0200

    test: test DefaultRenderer

---
+++ src/renderers/default/_test/example1.mock.test.ts
@@ -0,0 +1,37 @@
+// tslint:disable
+
+/**
+ * https://steemit.com/steemit/@steemitblog/wallet-app-soft-roll-out-is-live
+ */
+
+export const raw = `
+![Major post copy.png](https://cdn.steemitimages.com/DQmTcGq5DXga8RbFeFjuzUXkLPBBi4MUk1WRUqgdnb4Dr6G/Major%20post%20copy.png)
+
+In our [engineering update released earlier today](https://steemit.com/steemit/@steemitblog/engineering-update-all-team-meeting-condenser-split) we announced that we would soft roll-out the new Wallet app. Well, we are now happy to announce the soft roll-out of the new Wallet application! To see what we mean, head on over to [steemitwallet.com](https://steemitwallet.com). For the time being, a full version of Condenser will be running on steemit.com, but once the Wallet app has been sufficiently tested, the new Social app will be rolled out to steemit.com. At that time, users who attempt to access their Steem wallet will be redirected to [steemitwallet.com](https://steemitwallet.com), so it was very important to maintain a consistent user experience. At that point, the Condenser Split will be complete.
+
+<h1>Check It Out</h1>
+
+The reason we are following this soft rollout strategy is so that users can begin familiarizing themselves with this new application, so be sure to head on over to [steemitwallet.com](https://steemitwallet.com) to see what the experience is like. One change you will see on steemit.com is that when you try to sign in with your Active or Owner Key, you will now receive the following warning:
+
+
+![login-with-posting-key](https://cdn.steemitimages.com/DQmRyYMhuAyRHjocXHPqTPByr7Gx8LubotqF2rwY6rgk996/logging-in-with-posting-key.png)
+
+The goal is to make the User Experience between the two applications as seamless as possible, so if you notice anything disruptive in this experience, please leave us feedback in the comments section below. 
+
+<h1>The Mission</h1>
+
+Our Mission at Steemit is to help build great online communities with financial inclusion. To that end, we envision a vibrant communities web application that features cryptocurrency incentives, the development of which is economically sustainable and scalable. Splitting Condenser into separate Social and Wallet applications will constitute a major step toward accomplishing our mission by enabling us to improve each application faster, consume fewer resources, and generate more revenue, while maximizing user security by enhancing the client-side signing experience.  
+
+_The Steemit Team_
+`;
+export const rendered = `
+<p><img src="https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmTcGq5DXga8RbFeFjuzUXkLPBBi4MUk1WRUqgdnb4Dr6G/Major%20post%20copy.png" alt="Major post copy.png"></p>
+<p>In our <a href="https://steemit.com/steemit/@steemitblog/engineering-update-all-team-meeting-condenser-split">engineering update released earlier today</a> we announced that we would soft roll-out the new Wallet app. Well, we are now happy to announce the soft roll-out of the new Wallet application! To see what we mean, head on over to <a href="https://steemitwallet.com" rel="nofollow noopener" title="This link will take you away from steemit.com">steemitwallet.com</a>. For the time being, a full version of Condenser will be running on steemit.com, but once the Wallet app has been sufficiently tested, the new Social app will be rolled out to steemit.com. At that time, users who attempt to access their Steem wallet will be redirected to <a href="https://steemitwallet.com" rel="nofollow noopener" title="This link will take you away from steemit.com">steemitwallet.com</a>, so it was very important to maintain a consistent user experience. At that point, the Condenser Split will be complete.</p>
+<p></p><h1>Check It Out</h1><p></p>
+<p>The reason we are following this soft rollout strategy is so that users can begin familiarizing themselves with this new application, so be sure to head on over to <a href="https://steemitwallet.com" rel="nofollow noopener" title="This link will take you away from steemit.com">steemitwallet.com</a> to see what the experience is like. One change you will see on steemit.com is that when you try to sign in with your Active or Owner Key, you will now receive the following warning:</p>
+<p><img src="https://steemitimages.com/640x0/https://cdn.steemitimages.com/DQmRyYMhuAyRHjocXHPqTPByr7Gx8LubotqF2rwY6rgk996/logging-in-with-posting-key.png" alt="login-with-posting-key"></p>
+<p>The goal is to make the User Experience between the two applications as seamless as possible, so if you notice anything disruptive in this experience, please leave us feedback in the comments section below.</p>
+<p></p><h1>The Mission</h1><p></p>
+<p>Our Mission at Steemit is to help build great online communities with financial inclusion. To that end, we envision a vibrant communities web application that features cryptocurrency incentives, the development of which is economically sustainable and scalable. Splitting Condenser into separate Social and Wallet applications will constitute a major step toward accomplishing our mission by enabling us to improve each application faster, consume fewer resources, and generate more revenue, while maximizing user security by enhancing the client-side signing experience.</p>
+<p><em>The Steemit Team</em></p>
+`;
---
+++ src/renderers/default/default-renderer-steemit-compatibility-test.spec.test.ts
@@ -0,0 +1,72 @@
+// tslint:disable no-console
+
+import { expect } from "chai";
+import { JSDOM } from "jsdom";
+import "mocha";
+
+import * as example1 from "./_test/example1.mock.test";
+import { DefaultRenderer } from "./DefaultRenderer";
+
+describe("DefaultRender", () => {
+    const defaultOptions: DefaultRenderer.Options = {
+        breaks: true,
+        skipSanitization: false,
+        addNofollowToLinks: true,
+        doNotShowImages: false,
+        ipfsPrefix: "",
+        assetsWidth: 640,
+        assetsHeight: 480,
+        imageProxyFn: (url: string) => url,
+    };
+
+    const tests = [
+        { name: "Renders H1 headers correctly", raw: `# Header H1`, expected: "<h1>Header H1</h1>" },
+        { name: "Renders H4 headers correctly", raw: `#### Header H4`, expected: "<h4>Header H1</h4>" },
+        {
+            name: "Renders headers and paragraphs correctly",
+            raw: "# Header H1\n\nSome paragraph\n\n## Header H2\n\nAnother paragraph",
+            expected: "<h1>Header H1</h1>\n<p>Some paragraph</p>\n<h2>Header H2</h2>\n<p>Another paragraph</p>",
+        },
+        {
+            name: "Renders steemit mentions correctly",
+            raw: "@noisy",
+            expected: "<h1>Header H1</h1>\n<p>Some paragraph</p>\n<h2>Header H2</h2>\n<p>Another paragraph</p>",
+        },
+    ];
+
+    tests.forEach(test =>
+        it(test.name, () => {
+            const renderer = new DefaultRenderer(defaultOptions);
+            const rendered = renderer.render(test.raw).trim();
+
+            console.log("=========== RENDERED ============");
+            console.log(rendered);
+            console.log();
+
+            console.log("=========== EXPECTED ============");
+            console.log(test.expected);
+            console.log();
+
+            const renderedNode = JSDOM.fragment(rendered);
+            const comparisonNode = JSDOM.fragment(test.expected);
+            expect(renderedNode.isEqualNode(comparisonNode)).to.be.equal(true);
+        }),
+    );
+
+    it("Produces equivalent to steemit.com", () => {
+        const renderer = new DefaultRenderer(defaultOptions);
+        const rendered = renderer.render(example1.raw);
+
+        console.log("=========== RENDERED ============");
+        console.log(rendered);
+        console.log();
+
+        console.log("=========== STEEMIT COMPARISON ============");
+        console.log(example1.rendered);
+        console.log();
+
+        const renderedNode = JSDOM.fragment(rendered);
+        const comparisonNode = JSDOM.fragment(example1.rendered);
+        expect(renderedNode.isEqualNode(comparisonNode)).to.be.equal(true);
+    });
+});
