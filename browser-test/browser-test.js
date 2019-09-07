53c59042dcf7e76df7078a839fb8d0b7346b1e37
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-09-07 14:17:58 +0200

    test: browser test #6 issue

--- browser-test/browser-test.js
+++ browser-test/browser-test.js
@@ -30,3 +30,11 @@ test('Renders properly simple markup', async t => {
     await t.click(Selector('#awaiter'))
     .expect(renderInBrowser({ ...defaultOptions }, markup)).eql('<h1>H1</h1>\n');
 });
+
+test('Does not crash on mixed-img markup', async t => {
+    const markup = `<img src="![Sacrifice The Truth Logo.jpg](https://cdn.steemitimages.com/DQmUjNstssuPJpjPDDWfRnw1x2tY6AWWKcajDMGpPLA5iJf/Sacrifice%20The%20Truth%20Logo.jpg)"/>`;
+    const expected = `<p><img src="brokenimg.jpg" /></p>\n`;
+
+    await t.click(Selector('#awaiter'))
+    .expect(renderInBrowser({ ...defaultOptions }, markup)).eql(expected);
+});
